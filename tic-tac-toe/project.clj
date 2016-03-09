1e5d1dec3939b7c275fe787060af1242e7a6570c
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-03-09 11:37:31 +0000

    Initial project setup

---
+++ tic-tac-toe/project.clj
@@ -0,0 +1,9 @@
+(defproject tic-tac-toe "0.1.0-SNAPSHOT"
+  :description "Clojure implementation of Tic Tac Toe"
+  :url "https://github.com/gemcfadyen/Apprenticeship-ClojureTicTacToe"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0-RC2"]]
+  :profiles {:dev {:dependencies [[speclj "3.3.1"]]}}
+  :plugins [[speclj "3.3.1"]]
+  :test-paths ["spec"])
---
+++ tic-tac-toe/spec/tic_tac_toe/writer_spec.clj
@@ -0,0 +1,7 @@
+(ns tic-tac-toe.writer-spec
+  (:require [speclj.core :refer :all]
+            [tic-tac-toe.writer :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 0 1)))
