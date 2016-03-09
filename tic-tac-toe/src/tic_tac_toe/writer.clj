9104e0e5fd6e2932db9d5cc342eb87f3f5e55337
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-03-09 12:02:16 +0000

    Writer displays information to the command line

--- tic-tac-toe/src/tic_tac_toe/writer.clj
+++ tic-tac-toe/src/tic_tac_toe/writer.clj
@@ -1,1 +1,15 @@
 (ns tic-tac-toe.writer)
+
+(defn prompt-for-next-move []
+  (println "Please enter your next move")
+
+  (defn invalid-input[]
+    (println "Invalid input!"))
+
+  (defn draw[]
+    (println "The game was a draw"))
+
+  (defn win[player-symbol]
+    (println "The game was won by" player-symbol)))
+
+
