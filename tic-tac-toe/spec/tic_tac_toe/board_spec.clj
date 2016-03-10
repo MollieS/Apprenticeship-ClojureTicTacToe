d065f0a9a8fb7b22429d7cab15673a89ef485803
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-03-10 12:15:41 +0000

    Format the board into rows for the prompt to display

--- tic-tac-toe/spec/tic_tac_toe/board_spec.clj
+++ tic-tac-toe/spec/tic_tac_toe/board_spec.clj
@@ -6,13 +6,28 @@
 (defn- empty-board[]
   (vec (repeat 9 nil)))
 
+(defn- partially-populated-board[]
+  [nil X O nil nil X nil O X])
+
 (describe "Board"
 
-(it "created with given configuration"
-    (should=[X O nil nil nil nil nil nil nil]
-               (create [X O nil nil nil nil nil nil nil])))
+          (it "created with given configuration"
+              (should= (partially-populated-board)
+                       (create [nil X O nil nil X nil O X])))
 
 
           (it "updates with players mark at given index"
               (should= [nil nil nil nil nil nil nil nil X]
-                       (place-mark (empty-board) X 8))))
+                       (place-mark (empty-board) X 8)))
+
+
+          (it "gets the players mark at a given index"
+              (should= X
+                       (get-mark-at-index (partially-populated-board) 5)))
+
+          (it "gets rows for display"
+              (should= [[nil X O][nil nil X][nil O X]]
+                       (get-rows (partially-populated-board)))))
+
+
+
--- tic-tac-toe/src/tic_tac_toe/board.clj
+++ tic-tac-toe/src/tic_tac_toe/board.clj
@@ -1,9 +1,14 @@
 (ns tic-tac-toe.board)
 
 (defn create [board-config]
-  board-config
-  )
+  board-config)
 
 (defn place-mark[board mark index]
-  (assoc board index mark)
+  (assoc board index mark))
+
+(defn get-mark-at-index [board index]
+  (get board index))
+
+(defn get-rows[board]
+  [(subvec board 0 3) (subvec board 3 6) (subvec board 6 9)]
   )
