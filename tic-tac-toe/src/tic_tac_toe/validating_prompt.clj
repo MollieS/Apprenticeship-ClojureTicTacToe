f4be272d310a726eba13ed9ddd4bf39ff428c40c
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-03-15 16:17:35 +0000

    Renames boolean methods

--- tic-tac-toe/src/tic_tac_toe/validating_prompt.clj
+++ tic-tac-toe/src/tic_tac_toe/validating_prompt.clj
@@ -11,54 +11,41 @@
   (boolean (some #(= index %) spaces-on-board))
   )
 
-(defn- within-range? [provided-index board]
+(defn- show-invalid-message [error-msg board]
+  (writer/display board)
+  (error-msg))
+
+(defn- validate-input-is-within-range [provided-index board]
   (if (vacant-space provided-index (board/indicies-of-free-spaces board))
     true
-    false
-    )
+    (do
+      (show-invalid-message writer/invalid-space-message board)
+      false
+      ))
   )
 
 (defn- to-number [input]
   (Integer/parseInt input))
 
-(defn- numeric? [input]
+(defn- validate-input-is-numeric [input board]
   (try
     (to-number input)
     true
     (catch Exception e
+      (show-invalid-message writer/not-numeric-message board)
       false
       )
     ))
 
-(defn- show-invalid-message [error-msg board]
-  (writer/display board)
-  (error-msg)
-  )
+(defn valid-next-move[board]
+  (writer/prompt-for-next-move)
 
-(defn- not-numeric [input]
-  (not (numeric? input)))
+  (let [input (reader/read-input)]
 
-(defn- not-valid-space [input board]
-  (not (within-range? (zero-based (to-number input)) board)))
-
-  (defn valid-next-move[board]
-    (writer/prompt-for-next-move)
-
-    (let [input (reader/read-input)]
-
-      (if (not-numeric input)
-        (do
-          (show-invalid-message writer/not-numeric-message board)
-          (valid-next-move board)
-          )
-
-        (if (not-valid-space input board)
-          (do
-            (show-invalid-message writer/invalid-space-message board)
-            (valid-next-move board)
-            )
-          (zero-based (to-number input))
-          )
-        )
+    (if (and (validate-input-is-numeric input board)
+             (validate-input-is-within-range (zero-based (to-number input)) board))
+      (zero-based (to-number input))
+      (valid-next-move board)
       )
     )
+  )
