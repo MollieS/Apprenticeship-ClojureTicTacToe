7a024b19311a1c395e03b21d42b105d79e2551fa
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-03-15 10:33:45 +0000

    Smaller functions introduced for the play-move method

--- tic-tac-toe/spec/tic_tac_toe/ttt_spec.clj
+++ tic-tac-toe/spec/tic_tac_toe/ttt_spec.clj
@@ -17,18 +17,18 @@
 
           (it "game starts by displaying an empty board"
               (with-redefs [create (stub :create {:return (empty-board)})
-                            play (stub :play {:return "whole game is stubbed"})
+                            play-move (stub :play-move {:return "whole game is stubbed"})
                             display (stub :display {:return "Board is displayed"}) ]
                 (start)
 
                 (should-have-invoked :create {:times 1})
-                (should-have-invoked :play {:times 1})
+                (should-have-invoked :play-move {:times 1})
                 (should-have-invoked :display {:times 1})))
 
           (it "displays board with each move"
               (with-redefs [display (stub :display {:return "Board is displayed"})]
                 ( with-in-str "3\n4\n9\n"
-                  (play [X O nil nil X O O X nil]))
+                  (play-move [X O nil nil X O O X nil]))
                 (should-have-invoked :display {:times 3})
                 )
               )
@@ -36,7 +36,7 @@
           (it "checks for a win after each move"
               (with-redefs [winning-line?(stub :winning-line? {:return false})]
                 ( with-in-str "3\n4\n9\n"
-                  (play [X O nil nil X O O X nil]))
+                  (play-move [X O nil nil X O O X nil]))
                 (should-have-invoked :winning-line? {:times 3})
                 )
               )
@@ -44,7 +44,7 @@
           (it "checks for a draw after each move until a win takes place"
               (with-redefs [free-spaces?(stub :free-spaces? {:return true})]
                 ( with-in-str "3\n4\n9\n"
-                  (play [X O nil nil X O O X nil]))
+                  (play-move [X O nil nil X O O X nil]))
                 (should-have-invoked :free-spaces? {:times 2})
                 )
               )
@@ -52,15 +52,15 @@
           (it "announces win"
               (with-redefs [valid-next-move (stub :next-move {:return 3}) ]
                 (should-contain "The game was won by X"
-                                (with-out-str (play [X O nil nil O nil X X O])))))
+                                (with-out-str (play-move [X O nil nil O nil X X O])))))
 
           (it "announces draw"
               (with-redefs [valid-next-move (stub :next-move {:return 2})]
                 (should-contain "The game was a draw"
-                                (with-out-str (play [X O nil O X X O X O])))))
+                                (with-out-str (play-move [X O nil O X X O X O])))))
 
           (it "has players take turns until board is full"
               (should-contain "The game was a draw"
                               (with-in-str "1\n2\n3\n4\n5\n7\n6\n9\n8\n"
                                 (with-out-str
-                                  (play (empty-board)))))))
+                                  (play-move (empty-board)))))))
--- tic-tac-toe/src/tic_tac_toe/ttt.clj
+++ tic-tac-toe/src/tic_tac_toe/ttt.clj
@@ -5,22 +5,34 @@
             [tic-tac-toe.board :refer :all]
             [tic-tac-toe.marks :refer :all]))
 
-(defn play [board]
+(defn- empty-board []
+(create (vec (repeat 9 nil))))
+
+(defn- has-win? [board]
+  (winning-line? board))
+
+(defn- announce-win [board]
+  (win (winning-symbol board)))
+
+(defn- no-free-spaces? [board]
+(not (free-spaces? board)))
+
+(defn play-move [board]
   (let [updated-board (place-mark board (next-mark board) (valid-next-move board) )]
     (display updated-board)
     (cond
-      (winning-line? updated-board) (win (winning-symbol updated-board))
-      (not (free-spaces? updated-board)) (draw)
+      (has-win? updated-board) (announce-win updated-board)
+      (no-free-spaces? updated-board) (draw)
       :else
-       (play updated-board)
+       (play-move updated-board)
       )
     )
   )
 
 (defn start[]
-  (let [board (create (vec (repeat 9 nil)))]
+  (let [board (empty-board)]
     (display board)
-    (play board)))
+    (play-move board)))
 
 (defn -main[]
   (start))
