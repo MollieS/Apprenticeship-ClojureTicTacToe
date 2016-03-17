de64a61ac52eadc55f1466da024ab11e81de248e
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-03-17 14:51:53 +0000

    Random player chooses an empty space on the board

---
+++ tic-tac-toe/spec/tic_tac_toe/random_player_spec.clj
@@ -0,0 +1,19 @@
+(ns tic-tac-toe.random-player-spec
+  (:require [speclj.core :refer :all]
+            [tic-tac-toe.random-player :refer :all]
+            [tic-tac-toe.marks :refer :all]))
+
+(describe "Random Player"
+
+          (it "Chooses the only free space on the board"
+              (should= 7
+                       (choose-move [X O X X X O O nil O])
+                       ))
+
+          (it "Chooses a free space from a partially populated board"
+              (should-contain (choose-move [X O nil nil nil X O nil X])
+                              '(2 3 4 7)))
+
+          (it "Chooses a free space on an empty board"
+              (should-contain (choose-move (vec (repeat 9 nil)))
+                              (range 0 9))))
