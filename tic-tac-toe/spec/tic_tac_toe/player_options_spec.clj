915ef5e3871fa08b968b6d724135c512a671a576
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-03-17 14:54:09 +0000

    Adds random player as an option into the player options

--- tic-tac-toe/spec/tic_tac_toe/player_options_spec.clj
+++ tic-tac-toe/spec/tic_tac_toe/player_options_spec.clj
@@ -4,14 +4,14 @@
 
 (describe "Player Options"
 
-          (it "has one category"
-              (should= 1
+          (it "has 3 categories"
+              (should= 3
                        (count player-options)))
 
           (it "has display format"
-              (should= "(1) Human vs Human\n"
+              (should= "(1) Human vs Human\n(2) Random vs Human\n(3) Human vs Random\n"
                        (display)))
 
          (it "valid choices"
-          (should= '(1)
+          (should= '(1 2 3)
                   (valid-player-options))))
--- tic-tac-toe/src/tic_tac_toe/player_options.clj
+++ tic-tac-toe/src/tic_tac_toe/player_options.clj
@@ -1,7 +1,9 @@
 (ns tic-tac-toe.player-options)
 
 (def player-options
-  { 1 "Human vs Human"})
+  { 1 "Human vs Human"
+    2 "Random vs Human"
+    3 "Human vs Random"})
 
 (defn display[]
   (apply str (map (fn[[k v]] (str "(" k ") " v "\n")) player-options)))
