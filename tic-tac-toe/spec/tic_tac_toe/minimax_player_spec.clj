098e62aed6b3d054dedf44c4ebae38b47fcb7f63
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-03-24 16:08:39 +0000

    Scores game when there is a win or a draw

--- tic-tac-toe/spec/tic_tac_toe/minimax_player_spec.clj
+++ tic-tac-toe/spec/tic_tac_toe/minimax_player_spec.clj
@@ -3,16 +3,26 @@
             [tic-tac-toe.minimax-player :refer :all]
             [tic-tac-toe.marks :refer :all]))
 
+(def win-board [X O nil X O nil X nil nil])
+(def draw-board [X O X O X X O X O])
+
 (describe "Minimax Player"
 
           (it "scores 10 plus depth when they win"
-              (should= 14 (score [X O nil X O nil X nil nil] X)))
+              (should= 14 (score win-board X)))
 
           (it "scores -10 minus depth when opponent wins"
               (should= -14 (score [O X nil O X nil O nil nil] X)))
 
-         (it "scores 0 for a draw"
-            (should= 0 (score [X O X O X X O X O] X))
-             )
+          (it "scores 0 for a draw"
+              (should= 0 (score draw-board X))
+              )
 
+          (it "calculates score when there are no spaces left on the board"
+              (should-invoke score {:times 1}
+                             (choose-move draw-board)))
+
+          (it "calculates score when there is a win on the board"
+              (should-invoke score {:times 1}
+                             (choose-move win-board)))
           )
