91deea21c4f78ba0a6c539929abedf637661885a
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-03-24 16:58:26 +0000

    All tests added for minimax

--- tic-tac-toe/spec/tic_tac_toe/minimax_player_spec.clj
+++ tic-tac-toe/spec/tic_tac_toe/minimax_player_spec.clj
@@ -9,13 +9,13 @@
 (describe "Minimax Player"
 
           (it "scores 10 plus depth when they win"
-              (should= 14 (score win-board X)))
+              (should= 14 (score win-board X 4)))
 
           (it "scores -10 minus depth when opponent wins"
-              (should= -14 (score [O X nil O X nil O nil nil] X)))
+              (should= -14 (score [O X nil O X nil O nil nil] X 4)))
 
           (it "scores 0 for a draw"
-              (should= 0 (score draw-board X))
+              (should= 0 (score draw-board X 0))
               )
 
           (it "calculates score when there are no spaces left on the board"
@@ -24,5 +24,69 @@
 
           (it "calculates score when there is a win on the board"
               (should-invoke score {:times 1}
-                             (choose-move win-board)))
+                            (choose-move win-board)))
+
+          (it "takes winning move on top row"
+              (should= 0
+                       (choose-move [nil X X O O nil nil nil nil])))
+
+          (it "takes winning move on middle row"
+              (should= 5
+                       (choose-move [nil nil nil X X nil O O nil])))
+
+          (it "takes winning move on bottow row"
+              (should= 9
+                       (choose-move [nil O O nil nil nil X X nil])))
+
+          (it "takes winning move on left column"
+              (should= 3
+                       (choose-move [X nil O nil O nil X nil nil ])))
+
+          (it "takes winning move on middle column"
+              (should= 4
+                       (choose-move [nil X nil O nil nil X O nil])))
+
+          (it "takes wininng move on right column"
+              (should= 2
+                       (choose-move [nil nil nil nil O X nil O X])))
+
+          (it "takes winning move on first diagonal"
+              (should= 2
+                       (choose-move [X nil O nil nil O nil nil X])))
+
+          (it "takes winning move on second diagonal"
+              (should= 2
+                       (choose-move [nil nil nil nil X O X nil O])))
+
+          (it "blocks opponent on top row"
+              (should= 1
+                       (choose-move [O nil O X nil nil nil nil X])))
+
+          (it "blocks opponent on middle row"
+              (should= 3
+                       (choose-move [nil nil X nil O O nil X nil])))
+
+          (it "blocks opponent on bottom row"
+              (should= 7
+              (choose-move [nil nil X nil X nil O nil O])))
+
+          (it "blocks opponent on left column"
+              (should= 3
+                       (choose-move [O X nil nil nil X O nil nil])))
+
+          (it "blocks opponent in middle column"
+              (should= 7
+                       (choose-move [nil O nil X O nil nil nil X])))
+
+          (it "blocks opponent in right column"
+              (should= 5
+                       (choose-move [nil X O nil nil nil X nil O])))
+
+          (it "blocks opponent in first diagonal"
+              (should= 8
+                       (choose-move [O nil X nil O nil nil X nil])))
+
+          (it "blocks opponent in second diagonal"
+              (should= 4
+                       (choose-move [nil nil O nil nil X O nil X])))
           )
