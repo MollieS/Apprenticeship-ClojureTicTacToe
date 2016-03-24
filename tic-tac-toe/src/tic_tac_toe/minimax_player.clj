085956d83f9709214b0e14bc61b2b25db8c00bcd
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-03-24 16:58:26 +0000

    All tests added for minimax

--- tic-tac-toe/src/tic_tac_toe/minimax_player.clj
+++ tic-tac-toe/src/tic_tac_toe/minimax_player.clj
@@ -6,37 +6,48 @@
 (defn- minimax-wins? [board minimax-symbol]
   (= (board/winning-symbol board) minimax-symbol))
 
-(defn- score-opponent-win [board]
-  (- -10 (count (board/indicies-of-free-spaces board))))
+(defn- score-opponent-win [board remaining-depth]
+  (- -10 remaining-depth))
 
-(defn- score-minimax-win [board]
-  (+ 10  (count (board/indicies-of-free-spaces board))))
+(defn- score-minimax-win [board remaining-depth]
+  (+ 10 remaining-depth))
 
 (defn- draw? [board]
   (not (board/free-spaces? board)))
 
-(defn- score-for-win [board minimax-symbol]
+(defn- score-for-win [board minimax-symbol remaining-depth]
   (do
     (cond
-      (minimax-wins? board minimax-symbol) (score-minimax-win board)
+      (minimax-wins? board minimax-symbol) (score-minimax-win board remaining-depth)
       :else
-      (score-opponent-win board))))
+      (score-opponent-win board remaining-depth))))
 
-(defn score [board minimax-symbol]
+(defn score [board minimax-symbol remaining-depth]
   (cond
-    (board/winning-line? board) (score-for-win board minimax-symbol)
+    (board/winning-line? board) (score-for-win board minimax-symbol remaining-depth)
     (draw? board) 0))
 
 (defn- game-over? [board]
   (or (board/winning-line? board)
-            (draw? board)))
+      (draw? board)))
 
-(defn- determine-move [board minimax-symbol]
-    (if (game-over? board)
-      (score board minimax-symbol))
+(defn- calculate-initial-score [board minimax-symbol]
+  (if (= minimax-symbol (marks/next-mark board))
+    -100
+    100
+    )
+  )
+
+(defn- minimax [board minimax-symbol depth]
+  (let [best-score (calculate-initial-score board minimax-symbol)]
+
+    (if (or (game-over? board) (= 0 depth))
+    (score board minimax-symbol depth))
+
+    )
   )
 
 (defn choose-move [board]
-  (let [minimax-symbol (marks/next-mark board)]
-   (determine-move board minimax-symbol))
-  )
+  (let [minimax-symbol (marks/next-mark board)
+        [[best-score best-move]] (minimax board minimax-symbol (count (board/indicies-of-free-spaces board)))]
+    best-move))
