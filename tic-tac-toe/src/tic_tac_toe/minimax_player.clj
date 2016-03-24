3f6c6483504025b553078407dc0339c57d169d19
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-03-24 18:07:29 +0000

    Wip - scoring works, positions dont

--- tic-tac-toe/src/tic_tac_toe/minimax_player.clj
+++ tic-tac-toe/src/tic_tac_toe/minimax_player.clj
@@ -27,9 +27,10 @@
     (board/winning-line? board) (score-for-win board minimax-symbol remaining-depth)
     (draw? board) 0))
 
-(defn- game-over? [board]
+(defn- game-over? [board depth]
   (or (board/winning-line? board)
-      (draw? board)))
+      (draw? board)
+      (= 0 depth)))
 
 (defn- calculate-initial-score [board minimax-symbol]
   (if (= minimax-symbol (marks/next-mark board))
@@ -38,16 +39,38 @@
     )
   )
 
+(defn- get-best-position [is-minimax-player [best-score best-move] as best-pos [current-score current-move] as curr-pos current-position]
+  (if is-minimax-player
+    (if (< best-score current-score)
+      [current-score current-position]
+      best-pos)
+    (if (> best-score current-score)
+      [current-score current-position]
+      best-pos)
+    )
+  )
+
+; have tried to mimic my java one without pruning.
+; need to walk through this algorithm with the test cases to see what is wrong
 (defn- minimax [board minimax-symbol depth]
-  (let [best-score (calculate-initial-score board minimax-symbol)]
+  (let [init-best-position (calculate-initial-score board minimax-symbol)]
 
-    (if (or (game-over? board) (= 0 depth))
-    (score board minimax-symbol depth))
+    (if (game-over? board depth)
+      (score board minimax-symbol depth)
 
+    (let [ [h & t] (board/indicies-of-free-spaces board)
+          next-symbol (marks/next-mark board)
+          updated-board (board/place-mark board next-symbol (Integer/parseInt h))
+          position (minimax updated-board minimax-symbol (- depth 1) )]
+      (println "next symbol is " next-symbol)
+      (println "updated board is " updated-board)
+      (println "position " position)
+      (get-best-position (= minimax-symbol next-symbol) init-best-position position (Integer/parseInt h))
+      ))
     )
   )
 
 (defn choose-move [board]
-  (let [minimax-symbol (marks/next-mark board)
+  (let [ minimax-symbol (marks/next-mark board)
         [[best-score best-move]] (minimax board minimax-symbol (count (board/indicies-of-free-spaces board)))]
     best-move))
