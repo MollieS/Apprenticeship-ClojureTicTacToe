72876a99c3f6d5b5852d9ec29eab80a459e0c4e9
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-03-24 16:08:39 +0000

    Scores game when there is a win or a draw

--- tic-tac-toe/src/tic_tac_toe/minimax_player.clj
+++ tic-tac-toe/src/tic_tac_toe/minimax_player.clj
@@ -3,7 +3,6 @@
             [tic-tac-toe.board :as board]
             [tic-tac-toe.marks :as marks]))
 
-
 (defn- minimax-wins? [board minimax-symbol]
   (= (board/winning-symbol board) minimax-symbol))
 
@@ -27,3 +26,17 @@
   (cond
     (board/winning-line? board) (score-for-win board minimax-symbol)
     (draw? board) 0))
+
+(defn- game-over? [board]
+  (or (board/winning-line? board)
+            (draw? board)))
+
+(defn- determine-move [board minimax-symbol]
+    (if (game-over? board)
+      (score board minimax-symbol))
+  )
+
+(defn choose-move [board]
+  (let [minimax-symbol (marks/next-mark board)]
+   (determine-move board minimax-symbol))
+  )
